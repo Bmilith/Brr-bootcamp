1570891192fa128d9dbb04401193664b01d24998
Bmilith <96339417+Bmilith@users.noreply.github.com>
2021-12-18 22:42:40 +0100

    update-button
    3-init

--- task/button/src/components/button.js
+++ task/button/src/components/button.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import Style from "../style/button.module.css";
 
-function button() {
-  const GetNewColor = async (e) => {
-    e.preventDefault();
-    const res = await fetch("https://www.colr.org/json/color/random");
-    const data = await res.json();
-    console.log(data);
-  };
+function GetApi(colors) {
+  const [advice, setAdvice] = useState("");
+  const [history, sethistory] = useState("");
 
+  const url = "https://www.colr.org/json/color/random";
+  const fetchData = async (e) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+      console.log(history);
+      setAdvice(json.new_color);
+      sethistory((oldstate) => [
+        ...oldstate,
+        {
+          color: advice,
+        },
+      ]);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <div className={Style.container}>
-      <Button onClick={GetNewColor} variant="contained" endIcon={<SendIcon />}>
-        Send
+      <Button
+        style={{ color: `#${advice}` }}
+        onClick={fetchData}
+        variant="contained"
+        endIcon={<SendIcon />}
+      >
+        heloo
       </Button>
     </div>
   );
 }
 
-export default button;
+export default GetApi;
